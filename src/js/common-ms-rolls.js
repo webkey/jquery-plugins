05e89bd8869a47e681f77f2bf3a668d1d4764957
webkey <serhiy_i@ukr.net>
2019-02-12 11:26:36 +0300

    20190212 1125 ms-rolls

--- src/js/common-ms-rolls.js
+++ src/js/common-ms-rolls.js
@@ -5,51 +5,51 @@
 'use strict';
 
 $(function () {
-	// Тест переопределения настроек глобально
-	// $.fn.msRolls.defaultOptions.collapsed = false;
+  // Тест переопределения настроек глобально
+  // $.fn.msRolls.defaultOptions.collapsed = false;
 
-	var $rolls_1_1 = $('.rolls-js');
+  var $rolls_1_1 = $('.rolls-js');
 
-	if ($rolls_1_1.length) {
+  if ($rolls_1_1.length) {
 
-		var options = {
-			collapsed: true
-			// , modifiers: {
-			// 	activeClass: 'tab-poookkhh'
-			// }
-			, animationSpeed: 500
-			, accessibility: false // Включить переходы табом
-			// , event: 'mouseenter'
-			// , collapsed: false
-			, modifiers: {
-				activeClass: 'is-open'
-			}
-			, beforeOpen: function () {
-				console.log('.beforeOpen');
-			}
-			, afterOpen: function () {
-				console.log('.afterOpen');
-			}
-			, beforeClose: function () {
-				console.log('.beforeClose');
-			}
-			, afterClose: function () {
-				console.log('.afterClose');
-			}
-		};
+    var options = {
+      collapsed: true
+      // , modifiers: {
+      // 	activeClass: 'tab-poookkhh'
+      // }
+      , animationSpeed: 500
+      , accessibility: false // Включить переходы табом
+      // , event: 'mouseenter'
+      // , collapsed: false
+      , modifiers: {
+        activeClass: 'is-open'
+      }
+      , beforeOpen: function (e, el) {
+        console.log('.beforeOpen');
+      }
+      , afterOpen: function () {
+        console.log('.afterOpen');
+      }
+      , beforeClose: function () {
+        console.log('.beforeClose');
+      }
+      , afterClose: function () {
+        console.log('.afterClose');
+      }
+    };
 
-		var myRolls = $rolls_1_1.on('msRolls.afterInit', function () {
-			console.log('Plugin MS-Rolls has initialized!');
-		}).msRolls(options);
+    var myRolls = $rolls_1_1.on('msRolls.afterInit', function () {
+      console.log('Plugin MS-Rolls has initialized!');
+    }).msRolls(options);
 
-		// Тестирование метода "open"
-		// Первый параметр: название метода
-		// Второй параметр: селектор
-		// Трений параметр: callback-функция
-		$('.open-panel-js').on('click', function () {
-			myRolls.msRolls('open', $('#hashExpl'), function () {
-				// console.log('Показать после открытия панели!');
-			});
-		});
-	}
+    // Тестирование метода "open"
+    // Первый параметр: название метода
+    // Второй параметр: селектор
+    // Трений параметр: callback-функция
+    $('.open-panel-js').on('click', function () {
+      myRolls.msRolls('open', $('#hashExpl'), function () {
+        console.log('Панель #hashExpl открыта!');
+      });
+    });
+  }
 });
