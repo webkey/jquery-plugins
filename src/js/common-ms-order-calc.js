1f5609deb361ecf1c8505ee3e9a3cd90780cb739
webkey <serhiy_i@ukr.net>
2019-04-25 14:35:51 +0300

    Калькулятор корзины

--- src/js/common-ms-order-calc.js
+++ src/js/common-ms-order-calc.js
@@ -4,123 +4,124 @@
 
 $(function () {
 
-	/** ! инициализация спиннера */
-	initSpinner($('.spinner-js'));
+  /** ! инициализация спиннера */
+  initSpinner($('.spinner-js'));
 
-	function initSpinner (element) {
-		element.spinner({
-			min: 0
-		});
-	}
+  function initSpinner(element) {
+    element.spinner({
+      min: 0
+    });
+  }
 
-	/** ! only number input */
-	// link: https://stackoverflow.com/questions/995183/how-to-allow-only-numeric-0-9-in-html-inputbox-using-jquery
-	$("[data-only-number]").keydown(function (e) {
-		// Allow: backspace, delete, tab, escape, enter and .
-		if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 190]) !== -1 || // Allow: Ctrl+A, Command+A
-			(e.keyCode === 65 && (e.ctrlKey === true || e.metaKey === true)) || // Allow: home, end, left, right, down, up
-			(e.keyCode >= 35 && e.keyCode <= 40)) {
-			// let it happen, don't do anything
-			return;
-		}
-		// Ensure that it is a number and stop the keypress
-		if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
-			e.preventDefault();
-		}
-	});
+  /** ! only number input */
+  // link: https://stackoverflow.com/questions/995183/how-to-allow-only-numeric-0-9-in-html-inputbox-using-jquery
+  $("[data-only-number]").keydown(function (e) {
+    // Allow: backspace, delete, tab, escape, enter and .
+    if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 190]) !== -1 || // Allow: Ctrl+A, Command+A
+        (e.keyCode === 65 && (e.ctrlKey === true || e.metaKey === true)) || // Allow: home, end, left, right, down, up
+        (e.keyCode >= 35 && e.keyCode <= 40)) {
+      // let it happen, don't do anything
+      return;
+    }
+    // Ensure that it is a number and stop the keypress
+    if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+      e.preventDefault();
+    }
+  });
 
-	/** ========================== */
-	/** ========================== */
+  /** ========================== */
+  /** ========================== */
 
-	/** !инициализация плагина */
+  /** !инициализация плагина */
 
-	var orderCalcOptions = {
-		row: '.c-tr'
-		//, objParams: {
-		// 	'P209101_155_44': {
-		// 		'count': 1,
-		// 		'price': 200,
-		// 		'priceSum': 200
-		// 	},
-		// 	'P209102_170_48': {
-		// 		'count': 5,
-		// 		'price': 200,
-		// 		'priceSum': 1000
-		// 	}
-		// }
+  var orderCalcOptions = {
+    row: '.c-tr'
+    //, objParams: {
+    // 	'P209101_155_44': {
+    // 		'count': 1,
+    // 		'price': 200,
+    // 		'priceSum': 200
+    // 	},
+    // 	'P209102_170_48': {
+    // 		'count': 5,
+    // 		'price': 200,
+    // 		'priceSum': 1000
+    // 	}
+    // }
 
-		//, created: function (e, el, param) {
-		// 	console.log("e: ", e);
-		// 	console.log("el: ", el);
-		// }
-		, getTotalResults: function (e, el, results) {
-			// console.log("results: ", results);
-			// console.log("getTotalResults ");
-			$(el).find('.order-calc__total-results-js').toggleClass('show', results.totalCount > 0);
-			$(el).find('.order-calc-btn').prop('disabled', !results.totalCount > 0);
-		}
-		// , createdObjParams: function (e, el, obj) {
-		// 	console.log("obj: ", obj);
-		// }
-		, showedWarningRemoveItem: function (e, el) {
-			// console.log("showedWarningRemoveItem");
-			$(el).find('.order-calc-btn').prop('disabled', true);
-		}
-		, canceledRemoveItem: function (e, el) {
-			// console.log("canceledRemoveItem ");
-			$(el).find('.order-calc-btn').prop('disabled', false);
-		}
-		// , removedItem: function () {
-		// 	console.log("removedItem ");
-		// }
-		// , removedAllItems: function () {
-		// 	console.log("callback: removedAllItems");
-		// }
-	};
+    //, created: function (e, el, param) {
+    // 	console.log("e: ", e);
+    // 	console.log("el: ", el);
+    // }
+    , getTotalResults: function (e, el, results) {
+      // console.log("results: ", results);
+      // console.log("getTotalResults ");
+      $(el).find('.order-calc__total-results-js').toggleClass('show', results.totalCount > 0);
+      $(el).find('.order-calc-btn').prop('disabled', !results.totalCount > 0);
+    }
+    // , createdObjParams: function (e, el, obj) {
+    // 	console.log("obj: ", obj);
+    // }
+    , showedWarningRemoveItem: function (e, el) {
+      // console.log("showedWarningRemoveItem");
+      $(el).find('.order-calc-btn').prop('disabled', true);
+    }
+    , canceledRemoveItem: function (e, el) {
+      // console.log("canceledRemoveItem ");
+      $(el).find('.order-calc-btn').prop('disabled', false);
+    }
+    // , removedItem: function () {
+    // 	console.log("removedItem ");
+    // }
+    // , removedAllItems: function () {
+    // 	console.log("callback: removedAllItems");
+    // }
+  };
 
-	$('.order-calc-js').msOrderCalc(orderCalcOptions)
-		.css('border', '4px solid green'); // для проверки jquery цепочки
+  $('.order-calc-js').msOrderCalc(orderCalcOptions)
+      .css('box-shadow', '0 5px 0 lightgreen'); // для проверки jquery цепочки
 
 
-	/** ! добавить таблицу динамически */
-	$(document).ready(function(){
-		setTimeout(function () {
-			$.ajax({
-				url: "ms-order-calc--ajax-temp.html",
-				cache: false,
-				dataType: 'html',
-				success: function (html) {
-					$("#orderCalcCreate").append(html);
-					initSpinner($('.spinner-js'));
+  /** ! добавить таблицу динамически */
+  $(document).ready(function () {
+    setTimeout(function () {
+      $.ajax({
+        url: "ms-order-calc--ajax-temp.html",
+        cache: false,
+        dataType: 'html',
+        success: function (html) {
+          $("#orderCalcCreate").append(html);
+          initSpinner($('.spinner-js'));
 
-					$("#orderCalcCreate").find('.order-calc-js').msOrderCalc(orderCalcOptions)
-						.css('border', '4px solid blue'); // для проверки jquery цепочки
-				}});
-		}, 1000)
-	});
+          $("#orderCalcCreate").find('.order-calc-js').msOrderCalc(orderCalcOptions)
+              .css('box-shadow', '0 5px 0 lightblue'); // для проверки jquery цепочки
+        }
+      });
+    }, 1000)
+  });
 
-	$('.order-calc-js--alt').msOrderCalc({
-		row: '.c-tr'
-		, getTotalResults: function (e, el, results) {
-			// console.log("results: ", results);
-			// console.log("getTotalResults ");
-			$(el).find('.order-calc__total-results-js').toggleClass('show', results.totalCount > 0);
-			$(el).find('.order-calc-btn').prop('disabled', !results.totalCount > 0);
-		}
-		, showedWarningRemoveItem: function (e, el) {
-			// console.log("showedWarningRemoveItem");
-			$(el).find('.order-calc-btn').prop('disabled', true);
-		}
-		, canceledRemoveItem: function (e, el) {
-			// console.log("canceledRemoveItem ");
-			$(el).find('.order-calc-btn').prop('disabled', false);
-		}
-		// , removedItem: function () {
-		// 	console.log("removedItem ");
-		// }
-		// , removedAllItems: function () {
-		// 	console.log("callback: removedAllItems");
-		// }
-	})
-		.css('border', '4px solid black'); // для проверки jquery цепочки
+  $('.order-calc-js--alt').msOrderCalc({
+    row: '.c-tr'
+    , getTotalResults: function (e, el, results) {
+      // console.log("results: ", results);
+      // console.log("getTotalResults ");
+      $(el).find('.order-calc__total-results-js').toggleClass('show', results.totalCount > 0);
+      $(el).find('.order-calc-btn').prop('disabled', !results.totalCount > 0);
+    }
+    , showedWarningRemoveItem: function (e, el) {
+      // console.log("showedWarningRemoveItem");
+      $(el).find('.order-calc-btn').prop('disabled', true);
+    }
+    , canceledRemoveItem: function (e, el) {
+      // console.log("canceledRemoveItem ");
+      $(el).find('.order-calc-btn').prop('disabled', false);
+    }
+    // , removedItem: function () {
+    // 	console.log("removedItem ");
+    // }
+    // , removedAllItems: function () {
+    // 	console.log("callback: removedAllItems");
+    // }
+  })
+      .css('box-shadow', '0 5px 0 lightcoral'); // для проверки jquery цепочки
 });
--- src/js/plugins/jquery.ms-order-calc.js
+++ src/js/plugins/jquery.ms-order-calc.js
@@ -4,271 +4,271 @@
  * Description: Calculation of quantity and price of goods
  */
 
-;(function($){
-	var defaults = {
-		spinner: '.order-calc__number-js',
-		price: '.order-calc__price-js',
-		priceSum: '.order-calc__price-sum-js',
-		btnRemove: '.order-calc__remove-js',
-		row: 'tr',
-		totalResult: '.order-calc__total-results-js',
-		totalCount: '.order-calc__counts-total-js',
-		totalPrice: '.order-calc__price-total-js',
-		warningRemove: false,
-		warningRemoveDelay: 3000,
-		objParams: {},
-		tplRemoveItem: '<div><div><span>Товар удален</span> <a class="order-calc__cancel-js">Отмена</a></div></div>',
-
-		classes: {
-			init: 'ms-order-calc--initialized',
-			hasNotItems: 'order-calc__hasnt-items'
-		}
-	};
+;(function ($) {
+  var defaults = {
+    spinner: '.order-calc__number-js',
+    price: '.order-calc__price-js',
+    priceSum: '.order-calc__price-sum-js',
+    btnRemove: '.order-calc__remove-js',
+    row: 'tr',
+    totalResult: '.order-calc__total-results-js',
+    totalCount: '.order-calc__counts-total-js',
+    totalPrice: '.order-calc__price-total-js',
+    warningRemove: false,
+    warningRemoveDelay: 3000,
+    objParams: {},
+    tplRemoveItem: '<div><div><span>Товар удален</span> <a class="order-calc__cancel-js">Отмена</a></div></div>',
+
+    classes: {
+      init: 'ms-order-calc--initialized',
+      hasNotItems: 'order-calc__hasnt-items'
+    }
+  };
 
-	function MsOrderCalc(element, options) {
-		var self = this;
+  function MsOrderCalc(element, options) {
+    var self = this;
 
-		self.config = $.extend(true, {}, defaults, options);
-
-		self.element = element;
+    self.config = $.extend(true, {}, defaults, options);
+
+    self.element = element;
 
-		self.callbacks();
-
-		self.calcAll();
-		self.changeNumber();
+    self.callbacks();
+
+    self.calcAll();
+    self.changeNumber();
 
-		self.removeTimeout = 0;
-		if(!self.config.warningRemove) {
-			self.immediateRemoveItem();
-		} else {
-			self.showWarningRemoveItem();
-			self.cancelRemoveItem();
-		}
+    self.removeTimeout = 0;
+    if (!self.config.warningRemove) {
+      self.immediateRemoveItem();
+    } else {
+      self.showWarningRemoveItem();
+      self.cancelRemoveItem();
+    }
 
-		self.init(); // create DOM structure of the plugins
-	}
+    self.init(); // create DOM structure of the plugins
+  }
 
-	/** track events */
-	MsOrderCalc.prototype.callbacks = function () {
-		var self = this;
-		$.each(self.config, function (key, value) {
-			if(typeof value === 'function') {
-				self.element.on(key + '.msOrderCalc', function (e, param) {
-					return value(e, self.element, param);
-				});
-			}
-		});
-	};
+  /** track events */
+  MsOrderCalc.prototype.callbacks = function () {
+    var self = this;
+    $.each(self.config, function (key, value) {
+      if (typeof value === 'function') {
+        self.element.on(key + '.msOrderCalc', function (e, param) {
+          return value(e, self.element, param);
+        });
+      }
+    });
+  };
 
-	MsOrderCalc.prototype.calcAll = function () {
-		// console.log('===calcAll===');
-		var self = this, counter = 0;
+  MsOrderCalc.prototype.calcAll = function () {
+    // console.log('===calcAll===');
+    var self = this, counter = 0;
 
-		var $spinner = self.element.find(self.config.spinner);
-		$.each($spinner, function () {
-			var $curSpinner = $(this);
-			var curSpinnerVal = +$curSpinner.val();
-			if(!curSpinnerVal) {
-				return;
-			}
-			counter++;
+    var $spinner = self.element.find(self.config.spinner);
+    $.each($spinner, function () {
+      var $curSpinner = $(this);
+      var curSpinnerVal = +$curSpinner.val();
+      if (!curSpinnerVal) {
+        return;
+      }
+      counter++;
 
-			self.createObjParams($curSpinner, curSpinnerVal);
-		});
+      self.createObjParams($curSpinner, curSpinnerVal);
+    });
 
-		// call the function to recalculate the total result, if there is at least one item
-		// counter && self.calcTotalResult();
-		self.calcTotalResult();
-	};
+    // call the function to recalculate the total result, if there is at least one item
+    // counter && self.calcTotalResult();
+    self.calcTotalResult();
+  };
 
-	MsOrderCalc.prototype.changeNumber = function () {
-		var self = this;
+  MsOrderCalc.prototype.changeNumber = function () {
+    var self = this;
 
-		self.element.on('change spin', self.config.spinner, function (e, ui) {
-			var $curSpinner = $(this);
+    self.element.on('change spin', self.config.spinner, function (e, ui) {
+      var $curSpinner = $(this);
 
-			var curSpinnerVal = ui ? ui.value : +$curSpinner.val();
+      var curSpinnerVal = ui ? ui.value : +$curSpinner.val();
 
-			self.createObjParams($curSpinner, curSpinnerVal);
+      self.createObjParams($curSpinner, curSpinnerVal);
 
-			self.calcTotalResult();
-		});
-	};
+      self.calcTotalResult();
+    });
+  };
 
-	MsOrderCalc.prototype.immediateRemoveItem = function () {
-		var self = this;
+  MsOrderCalc.prototype.immediateRemoveItem = function () {
+    var self = this;
 
-		self.element.on('click', self.config.btnRemove, function (e) {
+    self.element.on('click', self.config.btnRemove, function (e) {
 
-			var $curRow = $(this).closest(self.config.row);
-			var id = $curRow.find(self.config.spinner).data('id');
+      var $curRow = $(this).closest(self.config.row);
+      var id = $curRow.find(self.config.spinner).data('id');
 
-			// remove current item from object
-			delete self.config.objParams[id];
+      // remove current item from object
+      delete self.config.objParams[id];
 
-			self.calcTotalResult();
+      self.calcTotalResult();
 
-			self.removeItem($curRow);
+      self.removeItem($curRow);
 
-			e.preventDefault();
+      e.preventDefault();
 
-		});
-	};
+    });
+  };
 
-	MsOrderCalc.prototype.showWarningRemoveItem = function () {
-		var self = this;
+  MsOrderCalc.prototype.showWarningRemoveItem = function () {
+    var self = this;
 
-		self.element.on('click', self.config.btnRemove, function (e) {
+    self.element.on('click', self.config.btnRemove, function (e) {
 
-			var $curRow = $(this).closest(self.config.row);
-			var $curCells = $curRow.children();
-			var id = $curRow.find(self.config.spinner).data('id');
+      var $curRow = $(this).closest(self.config.row);
+      var $curCells = $curRow.children();
+      var id = $curRow.find(self.config.spinner).data('id');
 
-			// remove item with class ".order-calc__remove-row-js"
-			self.removeItem(self.element.find('.order-calc__remove-row-js'));
+      // remove item with class ".order-calc__remove-row-js"
+      self.removeItem(self.element.find('.order-calc__remove-row-js'));
 
-			$curRow.addClass('order-calc__remove-row-js');
-			$curCells.addClass('order-calc__remove-cell-js');
-			$curRow.append($(self.config.tplRemoveItem).clone().addClass('order-calc__remove-warning-js'));
+      $curRow.addClass('order-calc__remove-row-js');
+      $curCells.addClass('order-calc__remove-cell-js');
+      $curRow.append($(self.config.tplRemoveItem).clone().addClass('order-calc__remove-warning-js'));
 
-			// remove current item from object
-			delete self.config.objParams[id];
+      // remove current item from object
+      delete self.config.objParams[id];
 
-			self.calcTotalResult();
+      self.calcTotalResult();
 
-			// add callback showedWarningRemoveItem (after calcTotalResult)
-			self.element.trigger('showedWarningRemoveItem.msOrderCalc');
+      // add callback showedWarningRemoveItem (after calcTotalResult)
+      self.element.trigger('showedWarningRemoveItem.msOrderCalc');
 
-			clearTimeout(self.removeTimeout);
+      clearTimeout(self.removeTimeout);
 
-			self.removeTimeout = setTimeout(function () {
-				self.removeItem($curRow);
-			}, self.config.warningRemoveDelay);
+      self.removeTimeout = setTimeout(function () {
+        self.removeItem($curRow);
+      }, self.config.warningRemoveDelay);
 
-			e.preventDefault();
-			
-		});
-	};
+      e.preventDefault();
 
-	MsOrderCalc.prototype.cancelRemoveItem = function () {
-		var self = this;
+    });
+  };
 
-		self.element.on('click', '.order-calc__cancel-js', function (e) {
+  MsOrderCalc.prototype.cancelRemoveItem = function () {
+    var self = this;
 
-			// clear removeTimeout
-			clearInterval(self.removeTimeout);
+    self.element.on('click', '.order-calc__cancel-js', function (e) {
 
-			var $curRow = $(this).closest(self.config.row);
-			var $curCells = $curRow.children();
+      // clear removeTimeout
+      clearInterval(self.removeTimeout);
 
-			$curRow.removeClass('order-calc__remove-row-js');
-			$curCells.removeClass('order-calc__remove-cell-js');
-			$curRow.find('.order-calc__remove-warning-js').remove();
+      var $curRow = $(this).closest(self.config.row);
+      var $curCells = $curRow.children();
 
-			// add callback canceledRemoveItem (before calcAll)
-			self.element.trigger('canceledRemoveItem.msOrderCalc');
+      $curRow.removeClass('order-calc__remove-row-js');
+      $curCells.removeClass('order-calc__remove-cell-js');
+      $curRow.find('.order-calc__remove-warning-js').remove();
 
-			self.calcAll();
+      // add callback canceledRemoveItem (before calcAll)
+      self.element.trigger('canceledRemoveItem.msOrderCalc');
 
-			e.preventDefault();
-		});
-	};
+      self.calcAll();
 
-	MsOrderCalc.prototype.removeItem = function (row) {
-		// console.log('===removeItem===');
-		var self = this;
+      e.preventDefault();
+    });
+  };
 
-		var id = row.find(self.config.spinner).data('id');
-		var curElement = row.closest(self.element);
-		row.remove();
+  MsOrderCalc.prototype.removeItem = function (row) {
+    // console.log('===removeItem===');
+    var self = this;
 
-		// remove current item from object
-		delete self.config.objParams[id];
+    var id = row.find(self.config.spinner).data('id');
+    var curElement = row.closest(self.element);
+    row.remove();
 
-		self.calcAll();
+    // remove current item from object
+    delete self.config.objParams[id];
 
-		// add callback removedItem
-		self.element.trigger('removedItem.msOrderCalc');
+    self.calcAll();
 
-		// count number of items
-		var rowsLengths = curElement.find(self.config.price).length;
-		if(!rowsLengths){
-			// add callback removedAllItem
-			self.element.trigger('removedAllItems.msOrderCalc');
+    // add callback removedItem
+    self.element.trigger('removedItem.msOrderCalc');
 
-			curElement.addClass(self.config.classes.hasNotItems);
-		}
-	};
+    // count number of items
+    var rowsLengths = curElement.find(self.config.price).length;
+    if (!rowsLengths) {
+      // add callback removedAllItem
+      self.element.trigger('removedAllItems.msOrderCalc');
 
-	MsOrderCalc.prototype.createObjParams = function (spinner, count) {
-		var self = this;
+      curElement.addClass(self.config.classes.hasNotItems);
+    }
+  };
 
-		var $curRow = $(spinner).closest(self.config.row);
-		var $curPrice = $curRow.find(self.config.price);
-		var priceVal = $curPrice.data('price');
-		var priceValSum = Math.round(priceVal * count * 100) / 100;
+  MsOrderCalc.prototype.createObjParams = function (spinner, count) {
+    var self = this;
 
-		// create the object with id's item
-		var id = spinner.data('id');
-		self.config.objParams[id] = {
-			'count': count,
-			'price': priceVal,
-			'priceSum': priceValSum
-		};
+    var $curRow = $(spinner).closest(self.config.row);
+    var $curPrice = $curRow.find(self.config.price);
+    var priceVal = $curPrice.data('price');
+    var priceValSum = Math.round(priceVal * count * 100) / 100;
 
-		// add callback createdObjParams
-		self.element.trigger('createdObjParams.msOrderCalc', self.config.objParams);
+    // create the object with id's item
+    var id = spinner.data('id');
+    self.config.objParams[id] = {
+      'count': count,
+      'price': priceVal,
+      'priceSum': priceValSum
+    };
 
-		// add count items and sum price to DOM (data-attributes)
-		$curPrice.attr('data-count-sum', count);
-		$curPrice.attr('data-price-sum', priceValSum);
+    // add callback createdObjParams
+    self.element.trigger('createdObjParams.msOrderCalc', self.config.objParams);
 
-		// add current item's price sum to DOM
-		$curRow.find(self.config.priceSum).html(priceValSum);
-	};
+    // add count items and sum price to DOM (data-attributes)
+    $curPrice.attr('data-count-sum', count);
+    $curPrice.attr('data-price-sum', priceValSum);
 
-	MsOrderCalc.prototype.calcTotalResult = function () {
-		// console.log('===calcTotalResult===');
-		var self = this;
+    // add current item's price sum to DOM
+    $curRow.find(self.config.priceSum).html(priceValSum);
+  };
 
-		var totalCount = self.sumParam(self.config.objParams, 'count');
-		var totalPrice = self.sumParam(self.config.objParams, 'priceSum');
+  MsOrderCalc.prototype.calcTotalResult = function () {
+    // console.log('===calcTotalResult===');
+    var self = this;
 
-		// add callback getTotalResults
-		self.element.trigger('getTotalResults.msOrderCalc', {'totalCount': totalCount, 'totalPrice': totalPrice});
+    var totalCount = self.sumParam(self.config.objParams, 'count');
+    var totalPrice = self.sumParam(self.config.objParams, 'priceSum');
 
-		// add total results to DOM
-		self.element.find(self.config.totalCount).text(totalCount);
-		self.element.find(self.config.totalPrice).text(totalPrice);
-	};
+    // add callback getTotalResults
+    self.element.trigger('getTotalResults.msOrderCalc', {'totalCount': totalCount, 'totalPrice': totalPrice});
 
-	MsOrderCalc.prototype.sumParam = function (obj, param) {
+    // add total results to DOM
+    self.element.find(self.config.totalCount).text(totalCount);
+    self.element.find(self.config.totalPrice).text(totalPrice);
+  };
 
-		var result = 0;
-		var prop;
+  MsOrderCalc.prototype.sumParam = function (obj, param) {
 
-		for(prop in obj) {
-			result += obj[prop][param];
-		}
+    var result = 0;
+    var prop;
 
-		return Math.round(result*100)/100;
+    for (prop in obj) {
+      result += obj[prop][param];
+    }
 
-	};
+    return Math.round(result * 100) / 100;
 
-	MsOrderCalc.prototype.init = function () {
+  };
 
-		this.element.addClass(this.config.classes.init);
+  MsOrderCalc.prototype.init = function () {
 
-		this.element.trigger('created.msOrderCalc');
+    this.element.addClass(this.config.classes.init);
 
-	};
+    this.element.trigger('created.msOrderCalc');
 
-	$.fn.msOrderCalc = function (options) {
-		'use strict';
+  };
 
-		return this.each(function(){
-			new MsOrderCalc($(this), options);
-		});
+  $.fn.msOrderCalc = function (options) {
+    'use strict';
 
-	};
+    return this.each(function () {
+      new MsOrderCalc($(this), options);
+    });
+
+  };
 })(jQuery);
