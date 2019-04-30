6127daff09cadd0fa676d57dbf26298351cf39a4
webkey <serhiy_i@ukr.net>
2019-04-30 19:57:28 +0300

    ...

--- src/js/common-ms-switch-class.js
+++ src/js/common-ms-switch-class.js
@@ -5,127 +5,129 @@
 'use strict';
 
 $(function () {
-	// Для адаптива
-	// На примере навигации
-	var $nav = $('.nav-opener-js'),
-		nav;
-	if ($nav.length) {
-		nav = $nav.switchClass({
-			switchClassTo: $('.shutter--nav-js').add('.nav-overlay-js')
-			, modifiers: {
-				activeClass: 'nav-is-open',
-				stopRemoveClass: 'stop-nav-remove-class'
-			}
-			, cssScrollFixed: true
-			, beforeAdded: function () {
-				$('html').addClass('open-only-mob');
-				// open-only-mob - используется для адаптива
+  // Для адаптива
+  // На примере навигации
+  var $nav = $('.nav-opener-js_____'),
+      nav;
+  if ($nav.length) {
+    nav = $nav.switchClass({
+      switchClassTo: $('.shutter--nav-js').add('.nav-overlay-js')
+      , modifiers: {
+        activeClass: 'nav-is-open',
+        stopRemoveClass: 'stop-nav-remove-class'
+      }
+      , cssScrollFixed: true
+      , beforeAdded: function () {
+        $('html').addClass('open-only-mob');
+        // open-only-mob - используется для адаптива
 
-				// пример добавления классов с задержкой
-				var $curItem = $('.nav-js').children(), speed = 1000;
+        // пример добавления классов с задержкой
+        var $curItem = $('.nav-js').children(), speed = 1000;
 
-				$('.nav-js').prop('counter', 0).animate({
-					counter: $curItem.length
-				}, {
-					duration: speed,
-					easing: 'swing',
-					step: function (now) {
-						$curItem.eq(Math.round(now)).addClass('show-nav-item')
-					}
-				});
-			}
-			, beforeRemoved: function () {
-				$('html').removeClass('open-only-mob');
-				// open-only-mob - используется для адаптива
-				$('.nav-js').stop().children().removeClass('show-nav-item')
-			}
-		});
-	}
+        $('.nav-js').prop('counter', 0).animate({
+          counter: $curItem.length
+        }, {
+          duration: speed,
+          easing: 'swing',
+          step: function (now) {
+            $curItem.eq(Math.round(now)).addClass('show-nav-item')
+          }
+        });
+      }
+      , beforeRemoved: function () {
+        $('html').removeClass('open-only-mob');
+        // open-only-mob - используется для адаптива
+        $('.nav-js').stop().children().removeClass('show-nav-item')
+      }
+    });
+  }
 
-	// Первый свитчер
-	var $switcher1 = $('.tc-js'),
-		tc1;
-	if ($switcher1.length) {
-		var options = {
-			// /*options*/
-			switcher: '.tc__switcher-js'
-			, adder: '.tc__opener-js'
-			, remover: '.tc__remover-js'
-			, switchClassTo: $('.tc__popup-js').add('.tc__overlay-js')
-			, modifiers: {
-				activeClass: 'active'
-			}
-			, cssScrollFixed: false
-			, removeOutsideClick: true
-		};
-		// tc1 = $switcher1.switchClass(options);
-		setTimeout(function () {
-			// Добавить кнопку(и) динамически
-			$('#add-switcher').html('<a href="#" class="tc__switcher-js"><span>Открыть попап 1 (switcher) (loaded)</span></a>');
-			tc1 = $switcher1.switchClass(options);
-		}, 1500);
-	}
+  // Первый свитчер
+  var $switcher1 = $('.tc-js'),
+      tc1;
+  if ($switcher1.length) {
+    var options = {
+      // /*options*/
+      switcher: '.tc__switcher-js'
+      , adder: '.tc__opener-js'
+      , remover: '.tc__remover-js'
+      , switchClassTo: $('.tc__popup-js').add('.tc__overlay-js')
+      , modifiers: {
+        activeClass: 'active'
+      }
+      , cssScrollFixed: false
+      , removeOutsideClick: true
+    };
 
+    // $switcher1.switchClass();
 
-	// Второй свитчер
-	var $switcher2 = $('.tc-2-js'),
-		tc2;
-	if ($switcher2.length) {
-		tc2 = $switcher2.switchClass({
-			remover: '.tc-2__remover-js'
-			, adder: '.tc-2__opener-js'
-			, switchClassTo: $('.tc-2__popup-js').add('.tc__overlay-js')
-			, modifiers: {
-				activeClass: 'is-open'
-			}
-			, cssScrollFixed: true
-			, beforeAdded: function () {
-				// console.log('beforeAdded 2');
-			}
-			, afterRemoved: function () {
-				// console.log('afterRemoved 2');
-			}
-		});
-	}
+    // tc1 = $switcher1.switchClass(options);
+    setTimeout(function () {
+      // Добавить кнопку(и) динамически
+      $('#add-switcher').html('<a href="#" class="tc__switcher-js"><span>Открыть попап 1 (switcher) (loaded)</span></a>');
+      tc1 = $switcher1.switchClass(options).css('border', '5px solid red');
+    }, 1500);
+  }
 
-	setTimeout(function () {
-		$('#add-switcher-02').html('<a href="#" class="tc-3-js"><span>Открыть попап 3 (loaded)</span></a>');
+  // Второй свитчер
+  var $switcher2 = $('.tc-2-js_____'),
+      tc2;
+  if ($switcher2.length) {
+    tc2 = $switcher2.switchClass({
+      remover: '.tc-2__remover-js'
+      , adder: '.tc-2__opener-js'
+      , switchClassTo: $('.tc-2__popup-js').add('.tc__overlay-js')
+      , modifiers: {
+        activeClass: 'is-open'
+      }
+      , cssScrollFixed: true
+      , beforeAdded: function () {
+        // console.log('beforeAdded 2');
+      }
+      , afterRemoved: function () {
+        // console.log('afterRemoved 2');
+      }
+    });
+  }
 
-		// Трений свитчер. Добавляется на страницу динамически
-		var $switcher3 = $('.tc-3-js');
-		var tc3 = $switcher3.switchClass({
-			switcher: '.tc-3__switcher-js'
-			, remover: '.tc-3__remover-js'
-			, switchClassTo: $('.tc-3__popup-js').add('.tc__overlay-js')
-			, modifiers: {
-				activeClass: 'active'
-			}
-			, cssScrollFixed: true
-			, beforeAdded: function () {
-				// console.log('beforeAdded 3');
-				// Если нужно удалять уже добавленные классы одного экземпляра плагина, при добавлении другого
-				tc1.switchClass('remove');
-				tc2.switchClass('remove');
-			}
-			, afterRemoved: function () {
-				// console.log('afterRemoved 3');
-			}
-		});
+  setTimeout(function () {
+    $('#add-switcher-02').html('<a href="#" class="tc-3-js"><span>Открыть попап 3 (loaded)</span></a>');
 
-		tc2.on('switchClass.beforeAdded', function () {
-			tc1.switchClass('remove');
-			tc3.switchClass('remove');
-			nav.switchClass('remove');
-		});
-		tc1.on('switchClass.beforeAdded', function () {
-			tc2.switchClass('remove');
-			tc3.switchClass('remove');
-			nav.switchClass('remove');
-		});
-		nav.on('switchClass.beforeAdded', function () {
-			tc1.switchClass('remove');
-			tc2.switchClass('remove');
-			tc3.switchClass('remove');
-		});
-	}, 1500);
+    // Трений свитчер. Добавляется на страницу динамически
+    var $switcher3 = $('.tc-3-js_____');
+    var tc3 = $switcher3.switchClass({
+      switcher: '.tc-3__switcher-js'
+      , remover: '.tc-3__remover-js'
+      , switchClassTo: $('.tc-3__popup-js').add('.tc__overlay-js')
+      , modifiers: {
+        activeClass: 'active'
+      }
+      , cssScrollFixed: true
+      , beforeAdded: function () {
+        // console.log('beforeAdded 3');
+        // Если нужно удалять уже добавленные классы одного экземпляра плагина, при добавлении другого
+        tc1.switchClass('remove');
+        tc2.switchClass('remove');
+      }
+      , afterRemoved: function () {
+        // console.log('afterRemoved 3');
+      }
+    });
+
+    // tc2.on('switchClass.beforeAdded', function () {
+    // 	tc1.switchClass('remove');
+    // 	tc3.switchClass('remove');
+    // 	nav.switchClass('remove');
+    // });
+    // tc1.on('switchClass.beforeAdded', function () {
+    // 	tc2.switchClass('remove');
+    // 	tc3.switchClass('remove');
+    // 	nav.switchClass('remove');
+    // });
+    // nav.on('switchClass.beforeAdded', function () {
+    // 	tc1.switchClass('remove');
+    // 	tc2.switchClass('remove');
+    // 	tc3.switchClass('remove');
+    // });
+  }, 1500);
 });
--- src/js/plugins/jquery.ms-switch-class.js
+++ src/js/plugins/jquery.ms-switch-class.js
@@ -5,226 +5,224 @@
  */
 
 ;(function ($) {
-	'use strict';
+  'use strict';
 
-	var countFixedScroll = 0;
-	// Нужно для корректной работы с доп. классом фиксирования скролла
+  var countFixedScroll = 0;
+  // Нужно для корректной работы с доп. классом фиксирования скролла
 
-	var SwitchClass = function (element, config) {
-		var self,
-			$element = $(element),
-			$html = $('html'),
-			pref = 'jq-switch-class',
-			pluginClasses = {
-				initClass: pref + '_initialized'
-			},
-			mod = {
-				scrollFixedClass: 'css-scroll-fixed'
-			},
-			$switchClassTo = $element.add(config.switcher).add(config.adder).add(config.remover).add(config.switchClassTo),
-			classIsAdded = false; //Флаг отвечающий на вопрос: класс добавлен?
+  var SwitchClass = function (element, config) {
+    var self,
+        $element = $(element),
+        $html = $('html'),
+        pref = 'jq-switch-class',
+        pluginClasses = {
+          initClass: pref + '_initialized'
+        },
+        mod = {
+          scrollFixedClass: 'css-scroll-fixed'
+        },
+        $switchClassTo = $element.add(config.switcher).add(config.adder).add(config.remover).add(config.switchClassTo),
+        classIsAdded = false; //Флаг отвечающий на вопрос: класс добавлен?
 
-		var callbacks = function () {
-				/** track events */
-				$.each(config, function (key, value) {
-					if (typeof value === 'function') {
-						$element.on('switchClass.' + key, function (e, param) {
-							return value(e, $element, param);
-						});
-					}
-				});
-			},
-			prevent = function (event) {
-				event.preventDefault();
-				event.stopPropagation();
-				return false;
-			},
-			toggleFixedScroll = function () {
-				$html.toggleClass(mod.scrollFixedClass, !!countFixedScroll);
-			},
-			add = function () {
-				if (classIsAdded) return;
+    var callbacks = function () {
+          /** track events */
+          $.each(config, function (key, value) {
+            if (typeof value === 'function') {
+              $element.on('switchClass.' + key, function (e, param) {
+                return value(e, $element, param);
+              });
+            }
+          });
+        },
+        prevent = function (event) {
+          event.preventDefault();
+          event.stopPropagation();
+          return false;
+        },
+        toggleFixedScroll = function () {
+          $html.toggleClass(mod.scrollFixedClass, !!countFixedScroll);
+        },
+        add = function () {
+          if (classIsAdded) return;
 
-				// Callback before added class
-				$element.trigger('switchClass.beforeAdded');
+          // Callback before added class
+          $element.trigger('switchClass.beforeAdded');
 
-				// Добавить активный класс на:
-				// 1) Основной элемент
-				// 2) Дополнительный переключатель
-				// 3) Элементы указанные в настройках экземпляра плагина
-				$switchClassTo.addClass(config.modifiers.activeClass);
+          // Добавить активный класс на:
+          // 1) Основной элемент
+          // 2) Дополнительный переключатель
+          // 3) Элементы указанные в настройках экземпляра плагина
+          $switchClassTo.addClass(config.modifiers.activeClass);
 
-				classIsAdded = true;
+          classIsAdded = true;
 
-				if (config.cssScrollFixed) {
-					// Если в настойках указано, что нужно добавлять класс фиксации скролла,
-					// То каждый раз вызывая ДОБАВЛЕНИЕ активного класса, увеличивается счетчик количества этих вызовов
-					++countFixedScroll;
-					toggleFixedScroll();
-				}
+          if (config.cssScrollFixed) {
+            // Если в настойках указано, что нужно добавлять класс фиксации скролла,
+            // То каждый раз вызывая ДОБАВЛЕНИЕ активного класса, увеличивается счетчик количества этих вызовов
+            ++countFixedScroll;
+            toggleFixedScroll();
+          }
 
-				// callback after added class
-				$element.trigger('switchClass.afterAdded');
-			},
-			remove = function () {
-				if (!classIsAdded) return;
+          // callback after added class
+          $element.trigger('switchClass.afterAdded');
+        },
+        remove = function () {
+          if (!classIsAdded) return;
 
-				// callback beforeRemoved
-				$element.trigger('switchClass.beforeRemoved');
+          // callback beforeRemoved
+          $element.trigger('switchClass.beforeRemoved');
 
-				// Удалять активный класс с:
-				// 1) Основной элемент
-				// 2) Дополнительный переключатель
-				// 3) Элементы указанные в настройках экземпляра плагина
-				$switchClassTo.removeClass(config.modifiers.activeClass);
+          // Удалять активный класс с:
+          // 1) Основной элемент
+          // 2) Дополнительный переключатель
+          // 3) Элементы указанные в настройках экземпляра плагина
+          $switchClassTo.removeClass(config.modifiers.activeClass);
 
-				classIsAdded = false;
+          classIsAdded = false;
 
-				if (config.cssScrollFixed) {
-					// Если в настойках указано, что нужно добавлять класс фиксации скролла,
-					// То каждый раз вызывая УДАЛЕНИЕ активного класса, уменьшается счетчик количества этих вызовов
-					--countFixedScroll;
-					toggleFixedScroll();
-				}
+          if (config.cssScrollFixed) {
+            // Если в настойках указано, что нужно добавлять класс фиксации скролла,
+            // То каждый раз вызывая УДАЛЕНИЕ активного класса, уменьшается счетчик количества этих вызовов
+            --countFixedScroll;
+            toggleFixedScroll();
+          }
 
-				// callback afterRemoved
-				$element.trigger('switchClass.afterRemoved');
-			},
-			events = function () {
-				$element.on('click', function (event) {
-					if (classIsAdded) {
-						remove();
+          // callback afterRemoved
+          $element.trigger('switchClass.afterRemoved');
+        },
+        events = function () {
+          $element.on('click', function (event) {
+            if (classIsAdded) {
+              remove();
 
-						event.preventDefault();
-						return false;
-					}
+              event.preventDefault();
+              return false;
+            }
 
-					add();
+            add();
 
-					prevent(event);
-				});
+            prevent(event);
+          });
 
-				$(config.switcher).on('click', function (event) {
-					$element.click();
-					prevent(event);
-				});
+          $(config.switcher).on('click', function (event) {
+            $element.click();
+            prevent(event);
+          });
 
-				$(config.adder).on('click', function (event) {
-					add();
-					prevent(event);
-				});
+          $(config.adder).on('click', function (event) {
+            add();
+            prevent(event);
+          });
 
-				$(config.remover).on('click', function (event) {
-					remove();
-					prevent(event);
-				})
+          $(config.remover).on('click', function (event) {
+            remove();
+            prevent(event);
+          })
 
-			},
-			removeByClickOutside = function () {
-				$html.on('click', function (event) {
-					if (classIsAdded && config.removeOutsideClick && !$(event.target).closest('.' + config.modifiers.stopRemoveClass).length) {
-						remove();
-						// event.stopPropagation();
-					}
-				});
-			},
-			removeByClickEsc = function () {
-				$html.keyup(function (event) {
-					if (classIsAdded && config.removeEscClick && event.keyCode === 27) {
-						remove();
-					}
-				});
-			},
-			init = function () {
-				$element.addClass(pluginClasses.initClass).addClass(config.modifiers.initClass);
-				$element.trigger('switchClass.afterInit');
-			};
+        },
+        removeByClickOutside = function () {
+          $html.on('click', function (event) {
+            if (classIsAdded && config.removeOutsideClick && !$(event.target).closest('.' + config.modifiers.stopRemoveClass).length) {
+              remove();
+              // event.stopPropagation();
+            }
+          });
+        },
+        removeByClickEsc = function () {
+          $html.keyup(function (event) {
+            if (classIsAdded && config.removeEscClick && event.keyCode === 27) {
+              remove();
+            }
+          });
+        },
+        init = function () {
+          $element.addClass(pluginClasses.initClass).addClass(config.modifiers.initClass);
+          $element.trigger('switchClass.afterInit');
+        };
 
-		self = {
-			callbacks: callbacks,
-			remove: remove,
-			events: events,
-			removeByClickOutside: removeByClickOutside,
-			removeByClickEsc: removeByClickEsc,
-			init: init
-		};
+    self = {
+      callbacks: callbacks,
+      remove: remove,
+      events: events,
+      removeByClickOutside: removeByClickOutside,
+      removeByClickEsc: removeByClickEsc,
+      init: init
+    };
 
-		return self;
-	};
+    return self;
+  };
 
-	// $.fn.switchClass = function (options, params) {
-	$.fn.switchClass = function () {
-		var _ = this,
-			opt = arguments[0],
-			args = Array.prototype.slice.call(arguments, 1),
-			l = _.length,
-			i,
-			ret;
-		for (i = 0; i < l; i++) {
-			if (typeof opt === 'object' || typeof opt === 'undefined') {
-				_[i].switchClass = new SwitchClass(_[i], $.extend(true, {}, $.fn.switchClass.defaultOptions, opt));
-				_[i].switchClass.callbacks();
-				_[i].switchClass.events();
-				_[i].switchClass.removeByClickOutside();
-				_[i].switchClass.removeByClickEsc();
-				_[i].switchClass.init();
-			}
-			else {
-				ret = _[i].switchClass[opt].apply(_[i].switchClass, args);
-			}
-			if (typeof ret !== 'undefined') {
-				return ret;
-			}
-		}
-		return _;
-	};
+  $.fn.switchClass = function () {
+    var _ = this,
+        opt = arguments[0],
+        args = Array.prototype.slice.call(arguments, 1),
+        l = _.length,
+        i,
+        ret;
+    // console.log("this: ", _);
+    console.log("opt: ", opt);
+    // console.log("l: ", l);
+    // console.log("args: ", args);
+    for (i = 0; i < l; i++) {
+      if (typeof opt === 'object' || typeof opt === 'undefined') {
+        _[i].switchClass = new SwitchClass(_[i], $.extend(true, {}, $.fn.switchClass.defaultOptions, opt));
+        _[i].switchClass.callbacks();
+        _[i].switchClass.events();
+        _[i].switchClass.removeByClickOutside();
+        _[i].switchClass.removeByClickEsc();
+        _[i].switchClass.init();
+      } else {
+        ret = _[i].switchClass[opt].apply(_[i].switchClass, args);
+      }
+      if (typeof ret !== 'undefined') {
+        return ret;
+      }
+    }
+    return _;
+  };
 
-	$.fn.switchClass.defaultOptions = {
-		switcher: null,
-		/**
-		 * @description - Дополнительный элемент, которым можно ДОБАВЛЯТЬ/УДАЛЯТЬ класс
-		 * @example {String}{JQ Object} null - '.switcher-js', или $('.switcher-js')
-		 */
-		adder: null,
-		/**
-		 * @description - Дополнительный элемент, которым можно ДОБАВЛЯТЬ класс
-		 * @example {String}{JQ Object} null - '.adder-js', или $('.adder-js')
-		 */
-		remover: null,
-		/**
-		 * @description - Дополнительный элемент, которым можно УДАЛЯТЬ класс
-		 * @example {String}{JQ Object} null - '.remover-js', или $('.remover-js')
-		 */
-		switchClassTo: null,
-		/**
-		 * @description - Один или несколько эелментов, на которые будет добавляться/удаляться активный класс (modifiers.activeClass)
-		 * @example {JQ Object} null - 1) $('html, .popup-js, .overlay-js')
-		 * @example {JQ Object} null - 2) $('html').add('.popup-js').add('.overlay-js')
-		 */
-		removeOutsideClick: true,
-		/**
-		 * @description - Удалать класс по клику по пустому месту на странице? Если по клику на определенный элемент удалять класс не нужно, то на этот элемент нужно добавить дата-антрибут [data-tc-stop]
-		 * @param {boolean} true - или false
-		 */
-		removeEscClick: true,
-		/**
-		 * @description - Удалять класс по клику на клавишу Esc?
-		 * @param {boolean} true - или false
-		 */
-		cssScrollFixed: false,
-		/**
-		 * @description - Добавлять на html дополнительный класс 'css-scroll-fixed'? Через этот класс можно фиксировать скролл методами css
-		 * @see - _mixins.sass =scroll-blocked()
-		 * @param {boolean} true - или false.
-		 */
-		modifiers: {
-			initClass: null,
-			activeClass: 'active',
-			stopRemoveClass: 'stop-remove-class' // Если кликнуть по елементу с этим классам, то событие удаления активного класса не будет вызвано
-		}
-		/**
-		 * @description - Список классов-модификаторов
-		 */
-	};
+  $.fn.switchClass.defaultOptions = {
+
+    // Remove existing classes
+    // Set this to false if you do not need to stack multiple instances
+    removeExisting: true,
+
+    // Дополнительный элемент, которым можно ДОБАВЛЯТЬ/УДАЛЯТЬ класс
+    // {String}{JQ Object} null - '.switcher-js', или $('.switcher-js')
+    adder: null,
+
+    // Дополнительный элемент, которым можно ДОБАВЛЯТЬ класс
+    // {String}{JQ Object} null - '.adder-js', или $('.adder-js')
+    remover: null,
+
+    // Дополнительный элемент, которым можно УДАЛЯТЬ класс
+    // {String}{JQ Object} null - '.remover-js', или $('.remover-js')
+    switchClassTo: null,
+
+    // Один или несколько эелментов, на которые будет добавляться/удаляться активный класс (modifiers.activeClass)
+    // {JQ Object} null - 1) $('html, .popup-js, .overlay-js')
+    // {JQ Object} null - 2) $('html').add('.popup-js').add('.overlay-js')
+    removeOutsideClick: true,
+
+    // Удалать класс по клику по пустому месту на странице? Если по клику на определенный элемент удалять класс не нужно, то на этот элемент нужно добавить дата-антрибут [data-tc-stop]
+    // {boolean} true - или false
+    removeEscClick: true,
+
+    // Удалять класс по клику на клавишу Esc?
+    // {boolean} true - или false
+    cssScrollFixed: false,
+
+    // Добавлять на html дополнительный класс 'css-scroll-fixed'? Через этот класс можно фиксировать скролл методами css
+    // _mixins.sass =scroll-blocked()
+    // {boolean} true - или false.
+    modifiers: {
+      initClass: null,
+      activeClass: 'active',
+      stopRemoveClass: 'stop-remove-class' // Если кликнуть по елементу с этим классам, то событие удаления активного класса не будет вызвано
+    },
+
+    // Список классов-модификаторов
+    switcher: null
+  };
 
 })(jQuery);
