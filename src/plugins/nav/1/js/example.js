0b5f02e9551d04b80bda863f5ffeb13a050860c5
webkey <serhiy_i@ukr.net>
2019-07-29 15:44:09 +0300

    Навигация. Стили

--- src/plugins/nav/1/js/example.js
+++ src/plugins/nav/1/js/example.js
@@ -6,8 +6,5 @@ $(function () {
         console.log("jQuery Plugin Created !!! It's fired afterInit event");
       }
     })
-        .css('padding', '1rem 2rem')
-        .css('background-color', 'lightblue')
-        .css('border', '2px solid darkblue');
   }
 });
